e65b3b094ab48793319c7c21ee59b4c3e83be6a8
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-07-16 22:41:13 +0100

    creating the ai choice and using switch case to allow ai to know what each number represent for it choices

--- Javascript/gameAi.js
+++ Javascript/gameAi.js
@@ -3,11 +3,38 @@ let r = document.getElementById("Rock")
 let p = document.getElementById("Paper") 
 let s = document.getElementById("Sissior")
 
+// Stores player Pick
 let player_Choice =  "" ; 
+// Stores player Pick
+let computer_Choice = "" ;
 
+//add eventlisten on each object and updates the player option 
 r.addEventListener("click", () => {
-    player_choice = "Rock"
-    console.log("rock")
+    player_Choice = "Rock"
+    let aiValue = aiChoiceGenerator()
+
+    if (aiValue == 1) {
+        computer_Choice = "Rock"
+    } else if (aiVlaue == 2) {
+        computer_Choice == "Paper"
+    } else if (aiValue == 3) {
+        computer_Choice = "Sissior"
+    }
+
+    // switch(aiValue) {
+    //     case 1 :
+    //         computer_Choice ="Rock"
+    //         break;
+    //     case 2:
+    //         computer_Choice ="Paper";
+    //         break;
+    //     case 3:
+    //         computer_Choice ="Sissior"
+    //         break;            
+    // }
+    console.log(aiValue)
+    // console.log(player_Choice)
+    console.log(computer_Choice )
 })
 
 p.addEventListener("click", () => {
@@ -19,3 +46,7 @@ s.addEventListener("click", () => {
     player_choice = "Sissor"
     console.log("sissior")
 })
+// get a number from 1 to 3 and round any decimal to the closet interger
+function aiChoiceGenerator() {
+ return Math.round((Math.random()*3))
+}
