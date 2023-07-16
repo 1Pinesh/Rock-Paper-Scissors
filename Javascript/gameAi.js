e2df25174cefbf318e1cb8779d00d03486e87256
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-07-16 22:04:14 +0100

    getting html element and putting them in variable. First evenlisten has been added and check to see if it work using consol

--- Javascript/gameAi.js
+++ Javascript/gameAi.js
@@ -1,1 +1,12 @@
-// console.log("Hello world");
+// Getting all the Elements 
+let r = document.getElementById("Rock")
+let p = document.getElementById("Papper") 
+let s = document.getElementById("Sissior")
+
+let player_Choice =  "" ; 
+
+r.addEventListener("click", () => {
+    player_choice = "Rock"
+    console.log("rock")
+})
+
